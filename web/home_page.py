28ea6c655aff6e7152edcfb9a6aad20a82eae0b9
afernandez-a <alejandro.fernandezarmas@gmail.com>
2024-01-23 22:11:43 +0100

    add info of the project on the webpage

--- web/home_page.py
+++ web/home_page.py
@@ -5,3 +5,24 @@ st.set_page_config(
     page_title="Home page",
     page_icon="👋",
     layout="centered")
+
+
+# Add a title
+st.title('Welcome to my points predictor!')
+
+# Add some text
+st.write("""
+This is a project that predict the points of a player in a specific gameweek
+
+## How it works
+
+The visualization page gives you the option to select a player or a team and plot some stats for the different gameweeks.
+The prediction page gives you the option to select a player and it will predict the points for the next gameweek.
+
+For more information, see the project documentation.
+""")
+
+# You can also use markdown
+st.markdown("""
+**Note:** This is a note written in markdown.
+""")
