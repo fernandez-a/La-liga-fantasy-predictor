697bb2a765d60b1c76f6f24280cb735a1c957b9a
afernandez-a <alejandro.fernandezarmas@gmail.com>
2024-01-23 19:51:11 +0100

    add data for the app

--- web/pages/prediction.py
+++ web/pages/prediction.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import matplotlib.pyplot as plt
+
 
 with open('rf_regressor.pkl', 'rb') as f:
     model = pickle.load(f)
 
 
 def load_data():
-    return pd.read_csv('./data/app_data.csv')
+    return pd.read_csv('./data/enhanced_data.csv')
     
 
 st.title("La liga Fantasy Football Predictor")
@@ -16,23 +18,62 @@ st.title("La liga Fantasy Football Predictor")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-
-    search_query = st.text_input("Search by player or team")
+    search_query = st.text_input("Search by player")
 
 with col2:
-
-    position_filter = st.selectbox("Select Position", options=["All", "GK", "DEF", "MID", "FWD"])
-
-with col3:
-    jornada_filter = st.select_slider("Rango de Jornada", options=range(1,5))  
+    position_filter = st.multiselect("Select Position", options=["All", "PO", "DF", "MC", "DE"])
 
 
 data = load_data()
+teamns = pd.read_csv('./data/teams_data.csv')
+players = pd.read_csv('./data/players_data.csv')
+
+data = data.drop_duplicates()
+
+latest_season = data['Temporada'].max()
+last_matchweek = data[data['Temporada'] == latest_season]['Jornada'].max()
+
+latest_data = data[(data['Temporada'] == latest_season) & 
+                            (data['Jornada'] == last_matchweek)]
+print(last_matchweek)
+
+def update_rolling_features(df, weeks=1):
+    for week in range(weeks):
+        for col in df.columns:
+            if 'last' in col:
+                num_weeks = last_matchweek
+                new_col = col.replace(f'last{num_weeks}w', f'last{num_weeks+1}w')
+                if new_col in df.columns:
+                    df[col] = df[new_col].shift(1)
+    return df
+
+weeks_to_forecast = 4
+forecast_datasets = {}
+
+for i in range(1, weeks_to_forecast + 1):
+    updated_data = update_rolling_features(latest_data.copy(), weeks=i)
+    updated_data.drop(['Jornada', 'Temporada','avg','tp'], axis=1, inplace=True)
+    updated_data.fillna(0, inplace=True)
+    forecast_datasets[f'week_{i}'] = updated_data
 
 
-filtered_search = data[data['nn'].str.contains(search_query, case=False, na=False)]
+week_1_data = forecast_datasets['week_4']
+player_ids = week_1_data['playerId'].values
+position_id = week_1_data['pid'].values
+team_ids = week_1_data['tid'].values
 
-col1, col2 = st.columns((1,1))
-with col1:
-    st.write(filtered_search['nn'].to_list()[0])
 
+week_1_predictions = model.predict(week_1_data)
+week_1_predictions = pd.DataFrame(week_1_predictions, columns=['Predicted Points'])
+week_1_predictions['playerId'] = player_ids
+week_1_predictions['tid'] = team_ids
+
+df_merged = week_1_predictions.merge(teamns, how='inner',on=['tid'])
+df_merged = df_merged.merge(players, how='inner',on=['playerId'])
+df_merged = df_merged[df_merged['nn'] != 'Vinícius Jr.']
+df_merged = df_merged.drop_duplicates()
+df_merged = df_merged.drop_duplicates(subset=['playerId'])
+
+if search_query:
+    df_merged = df_merged[df_merged['nn'].str.contains(search_query, case=False, na=False)]
+    df_merged[['nn', 'tn', 'Predicted Points']]
--- web/pages/visualization.py
+++ web/pages/visualization.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def load_data():
     return pd.read_csv('./data/app_data.csv')
@@ -9,14 +9,16 @@ def load_data():
 st.title("La liga Fantasy Football Predictor")
 
 data = load_data()
+data = data.drop_duplicates(subset=['Jornada', 'playerId','Temporada'])
+
 
 stats_dict = {
     'Goals': 'g',
     'Points': 'tp',
-    'Goals Against': 'ga',
-    'Position': 'position',
+    'Assists': 'ga',
+    'Shots converted': 'tsa',
     'Mins Played': 'mins',
-    'Read Cards': 'rc'
+    'Red Cards': 'rc',
 }
 
 data.loc[:,'position'] = data['pid'].map({1: 'PO', 2: 'DF', 3: 'MC', 4: 'DE'})
@@ -25,12 +27,11 @@ position_dict = {1: 'PO', 2: 'DF', 3: 'MC', 4: 'DE'}
 
 with st.sidebar:
     position_filter = st.multiselect("Select Position", options=["All", "PO", "DF", "MC", "DE"])
-    season_filter = st.multiselect("Select Season", options=["2021", "2022", "2023"])
+    season_filter = st.multiselect("Select Season", options=["2020","2021", "2022", "2023"])
     gw_range = st.slider("GW range", 1, 38, (22, 25))
     search_query = st.text_input("Input")
     options =  ['player','team']
-    multiselect_filter = st.multiselect("Custom Search",options)
-    statistics = st.multiselect("Statistics", options=['Goals', 'Goals Against'])#['Goals', 'Assists', 'Clean Sheets', 'Saves', 'Penalties Saved', 'Penalties Missed', 'Yellow Cards', 'Red Cards', 'Own Goals', 'Bonus Points', 'Points'])
+    multiselect_filter = st.selectbox("Custom Search",options)
     plot_stats = st.multiselect("Select Stats to Plot", options=list(stats_dict.keys()))
 
 filtered_data = data.copy()
@@ -52,43 +53,94 @@ if search_query:
         filtered_data = filtered_data[filtered_data['nn'].str.contains(search_query, case=False, na=False)]
     if 'team' in multiselect_filter:
         filtered_data = filtered_data[filtered_data['tn'].str.contains(search_query, case=False, na=False)]
-
-if statistics:
-    stats_columns = [stats_dict[stat] for stat in statistics]
-    filtered_data = filtered_data[stats_columns]    
-if plot_stats:
-    print(multiselect_filter)
+if plot_stats and search_query:
     stats_columns = [stats_dict[stat] for stat in plot_stats]
-    fig, axis = plt.subplots(2, 2, figsize=(15,15))
-    if plot_stats != None: 
-        if 'position' in stats_columns:
-            position_data = filtered_data['pid'].value_counts().reset_index()
-            position_data['index'] = position_data['pid'].map(position_dict) 
-            axis[0,0].pie(position_data['pid'], labels = position_data['index'],autopct='%1.1f%%')
-            axis[0,0].set_title('Position')
-        if 'mins' in stats_columns:
-            grouped_data = filtered_data.groupby('nn')['mins'].sum().sort_values(ascending=False).reset_index()
-            grouped_data.plot(x='nn', y='mins', kind='line', ax=axis[0,1])  # Adjusted axis index
-            axis[0,1].set_title('Mins')
-        if 'g' in stats_columns :
-            temp_data = filtered_data  # Use a temporary variable to avoid overwriting filtered_data
-            if not multiselect_filter:
-                temp_data = temp_data.sort_values(by=['g'], ascending=False).reset_index(drop=True)
-                temp_data = temp_data.head(60)
-            grouped_data = temp_data.groupby('nn')['g'].sum().sort_values(ascending=False).reset_index()
-            grouped_data.plot(x='nn', y='g', kind='bar', ax=axis[1,0])  # Adjusted axis index
-            axis[1,0].set_title('Goals')
-        if 'rc' in stats_columns :
-            temp_data = filtered_data
-            if not multiselect_filter:
-                temp_data = temp_data.sort_values(by=['rc'], ascending=False).reset_index(drop=True)
-                temp_data = temp_data.head(60)
-            grouped_data = temp_data.groupby('nn')['rc'].sum().sort_values(ascending=False).reset_index()
-            grouped_data.plot(x='nn', y='rc', kind='bar', ax=axis[1,1])  # Adjusted axis index
-            axis[1,1].set_title('Red Cards')
-    plt.tight_layout()
-    st.pyplot(fig)
+    if multiselect_filter == 'team' and search_query:
+        if not filtered_data.empty: 
+            fig, axis = plt.subplots(3, 2, figsize=(15,15))
+            if 'mins' in stats_columns:
+                temp_data = filtered_data.copy()
+                grouped_data = temp_data.groupby('nn')['mins'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='mins', kind='line', ax=axis[0,0])
+                axis[0,0].set_title('Mins')
+            if 'tp' in stats_columns :
+                temp_data = filtered_data.copy()  
+                grouped_data = temp_data.groupby('nn')['tp'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='tp', kind='bar', ax=axis[0,1], color='red')
+                axis[0,1].set_title('Total Points')
+            if 'ga' in stats_columns :
+                temp_data = filtered_data.copy()  
+                grouped_data = temp_data.groupby('nn')['ga'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='ga', kind='bar', ax=axis[1,0], color='green')
+                axis[1,0].set_title('Assists')
+            if 'tsa' in stats_columns :
+                temp_data = filtered_data.copy()
+                temp_data['%tsa_converted'] = temp_data['tsa'] / temp_data['g']
+                temp_data['%tsa_converted'] = temp_data['%tsa_converted'].replace([np.inf, -np.inf], np.nan)
+                temp_data['%tsa_converted'] = temp_data['%tsa_converted'].fillna(temp_data['%tsa_converted'].mean())
+                grouped_data = temp_data.groupby('nn')['%tsa_converted'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='%tsa_converted', kind='bar', ax=axis[1,1])
+                axis[1,1].set_title('% Shots Converted')
+            if 'g' in stats_columns :
+                temp_data = filtered_data.copy()  
+                grouped_data = temp_data.groupby('nn')['g'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='g', kind='bar', ax=axis[2,0])
+                axis[2,0].set_title('Goals')
+            if 'rc' in stats_columns :
+                temp_data = filtered_data.copy()
+                grouped_data = temp_data.groupby('nn')['rc'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='rc', kind='bar', ax=axis[2,1])
+                axis[2,1].set_title('Red Cards')
+            plt.tight_layout()
+            st.pyplot(fig)
+        else:
+            st.write("No data to plot")
+
+    elif multiselect_filter == 'player' and search_query:
+        if not filtered_data.empty:
+            fig, axis = plt.subplots(3, 2, figsize=(15,15))
+            if 'mins' in stats_columns:
+                temp_data = filtered_data.copy()
+                grouped_data = temp_data.groupby(['Jornada','nn'])['mins'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='mins', kind='line', ax=axis[0,0])
+                axis[0,0].set_title('Mins')
+            if 'tp' in stats_columns :
+                temp_data = filtered_data.copy()  
+                grouped_data = temp_data.groupby(['Jornada','nn'])['tp'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='tp', kind='bar', ax=axis[0,1], color='red')
+                axis[0,1].set_title('Total Points')
+            if 'ga' in stats_columns :
+                temp_data = filtered_data.copy()  
+                grouped_data = temp_data.groupby(['Jornada','nn'])['ga'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='ga', kind='bar', ax=axis[1,0], color='green')
+                axis[1,0].set_title('Assists')
+            if 'tsa' in stats_columns :
+                temp_data = filtered_data.copy()
+                temp_data['%tsa_converted'] = temp_data['tsa'] / temp_data['g']
+                temp_data['%tsa_converted'] = temp_data['%tsa_converted'].replace([np.inf, -np.inf], np.nan)
+                temp_data['%tsa_converted'] = temp_data['%tsa_converted'].fillna(temp_data['%tsa_converted'].mean())
+                grouped_data = temp_data.groupby(['Jornada','nn'])['%tsa_converted'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='%tsa_converted', kind='bar', ax=axis[1,1])
+                axis[1,1].set_title('% Shots Converted')
+            if 'g' in stats_columns :
+                temp_data = filtered_data.copy()  
+                grouped_data = temp_data.groupby(['Jornada','nn'])['g'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='g', kind='bar', ax=axis[2,0])
+                axis[2,0].set_title('Goals')
+            if 'rc' in stats_columns :
+                temp_data = filtered_data.copy()
+                grouped_data = temp_data.groupby(['Jornada','nn'])['rc'].sum().sort_values(ascending=False).reset_index()
+                grouped_data.plot(x='nn', y='rc', kind='bar', ax=axis[2,1], color='pink')
+                axis[2,1].set_title('Red Cards')
+            plt.tight_layout()
+            st.pyplot(fig)
+        else:
+            st.write("No data to plot")
+    else:
+        st.write("No data to plot")
+        
 
 
-filtered_data = filtered_data.sort_values(by=['Jornada']).reset_index(drop=True)
-st.dataframe(filtered_data)
+if not filtered_data.empty:
+    filtered_data = filtered_data.sort_values(by=['Jornada']).reset_index(drop=True)
+    st.dataframe(filtered_data)
